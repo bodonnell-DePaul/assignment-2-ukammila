3a59b69ed205a57ef3bb099576ae86623ad5ecd7
ukammila <ukammila@depaul.edu>
2024-10-07 22:11:03 -0500

    Update todoItems.js

--- todoItems.js
+++ todoItems.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { ListGroup, Tab, Row, Col, Container, Form, Button } from 'react-bootstrap';
 
 const TodoList = () => {
+  // Initial static array of todos
   const [todos, setTodos] = useState([
     { title: 'Todo 1', description: 'Lorem ipsum dolor sit amet...', dueDate: '2024-04-03' },
     { title: 'Todo 2', description: 'Sed auctor nunc nec nisi...', dueDate: '2024-04-06' },
@@ -9,13 +10,13 @@ const TodoList = () => {
     { title: 'Todo 4', description: 'Suspendisse potenti...', dueDate: '2024-04-11' },
   ]);
 
-  // Function to get Bootstrap variant based on due date
+  // Function to determine Bootstrap variant based on due date
   const getVariant = (dueDate) => {
     const currentDate = new Date();
     const dueDateObj = new Date(dueDate);
     const diffTime = dueDateObj - currentDate;
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    
+
     if (diffDays > 7) return 'primary';
     if (diffDays <= 7 && diffDays > 4) return 'success';
     if (diffDays <= 4 && diffDays > 2) return 'warning';
@@ -24,11 +25,13 @@ const TodoList = () => {
 
   return (
     <Container>
-      <h1>Assignment 2: Your Name ToDo List</h1>
+      <h1>Assignment 2: Brian's ToDo List</h1>
 
-      <Tab.Container id="todo-list" defaultActiveKey="#link1">
+      {/* Tab container for the ToDo items */}
+      <Tab.Container id="todo-list" defaultActiveKey="#link0">
         <Row>
           <Col sm={4}>
+            {/* ListGroup for todo titles */}
             <ListGroup>
               {todos.map((todo, index) => (
                 <ListGroup.Item
@@ -42,7 +45,9 @@ const TodoList = () => {
               ))}
             </ListGroup>
           </Col>
+
           <Col sm={8}>
+            {/* Tab content for todo descriptions */}
             <Tab.Content>
               {todos.map((todo, index) => (
                 <Tab.Pane eventKey={`#link${index}`} key={index}>
@@ -56,6 +61,7 @@ const TodoList = () => {
         </Row>
       </Tab.Container>
 
+      {/* Form to add a new ToDo item */}
       <h2>Add a new ToDo</h2>
       <Form>
         <Form.Group controlId="formTodoTitle">
